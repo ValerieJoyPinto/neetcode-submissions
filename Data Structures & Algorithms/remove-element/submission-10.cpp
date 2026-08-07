5b5f63d3dcc85aac8f6db7f76b4f340b766008f1
Valerie Joy Pinto <143357601+ValerieJoyPinto@users.noreply.github.com>
2026-08-07 13:07:31 -0700

    Add: remove-element - submission-10

---
+++ Data Structures & Algorithms/remove-element/submission-10.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0; // value to store
+
+        for(int j = 0; j < nums.size(); j++) {
+            if (nums[j] != val) {
+                nums[k] = nums[j];
+                k++;
+            }
+        }
+             return k;
+    }
+};
+
